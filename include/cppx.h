dda4302530516c193fb2952e70b3396db26e9091
IFKabir <ifk.ishanfalak@gmail.com>
2025-10-06 14:26:13 +0530

    added trim basic funtion

--- include/cppx.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-extern "C"
-{
-  int add(int a, int b);
-  int multiply(int a, int b);
-}
---
+++ include/trim.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+
+namespace cppx
+{
+  std::string trim(const std::string &str);
+}
